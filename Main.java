4056fd76ab5f75d4589de40e9dc49dc3a1e1d280
D0tsy <85434905+D0tsy@users.noreply.github.com>
2024-05-26 01:57:02 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,14 @@ class Tutor{
     public String specialization;
     public double hourly_rate;
     public list<String> reservations;
+
+    public Tutor(String name,String academicInfo,String specialization,double hourly_rate,){
+        this.name=name;
+        this.academicInfo=academicInfo;
+        this.specialization=specialization;
+        this.hourly_rate=hourly_rate;
+        this.reservations=new List<String>();
+    }
 }
 
 class Tutors{
