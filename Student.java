fca65fe8e309e760fb18864ad45884d422a4c000
LemonadeJuz <asyrafruit@gmail.com>
2024-05-26 20:59:39 +0800

    Student class

--- Student.java
+++ Student.java
@@ -15,6 +15,6 @@ public class Student {
         System.out.println("Stadium");
     }
     public static void phoneNum(){
-        System.out.println("12345");
+        System.out.println("123456");
     }
 }
