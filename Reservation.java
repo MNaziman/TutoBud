960cb2ed647ddc3e7ca2da513112f66a060050a5
MNaziman <mohamad_24000127@utp.edu.my>
2024-05-27 00:05:20 +0800

    Create Reservation.java

---
+++ Reservation.java
@@ -0,0 +1,27 @@
+public class Reservation
+{
+
+    public static void main(String[] args)
+    {
+        tutotime ();
+        tutodate ();
+        tutorname ();
+
+    }
+    public static int tutotime()
+    {
+
+        return 0;
+    }
+    public static int tutodate()
+    {
+
+        return 0;
+    }
+    public static int tutorname()
+    {
+
+
+        return 0;
+    }
+}
